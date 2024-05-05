c5df4aa2d8b4b1685f090b67ecd8424db6309d8a
Rux05 <rux05@users.noreply.github.com>
2024-05-05 13:01:25 +0400

    add style

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./body.scss";
 
 export default function Body() {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/properties")
+      .then((response) => response.json())
+      .then((data) => setProperties(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="body-container">
-      <h2>Welcome to our website!</h2>
-      <p>This is the main content of our page.</p>
-      <img src="./example.jpg" alt="Example" />
-      {/* Alte elemente sau componente pot fi adăugate aici */}
+      <img src="./image-source1.png" alt="Image source 1" className="banner" />
+      <h1>Chez vous, partout et ailleurs</h1>
+      <ul className="property-grid">
+        {properties.map((property) => (
+          <li key={property.id}>
+            <h2>{property.name}</h2>
+            <div className="property-image-container">
+              <img src={property.cover} alt={property.name} />
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import "./footer.scss"
-
+import React from "react";
+import "./footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
-    return (
-        <div className="footer-container">
-            {/* <Footer/> */}
-            <p>2020 Kasa. All rights reserved</p>
-        </div>
-    )
+  return (
+    <div className="footer-container">
+      {/* <Footer/> */}
+      <p>
+        {/* <FontAwesomeIcon icon={["far", "copyright"]} /> */}
+        2020 Kasa. All rights reserved
+      </p>
+    </div>
+  );
 }
