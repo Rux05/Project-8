52a8b9fd2c3e11c2161a7fb1e2edd7810f4802fb
Rux05 <rux05@users.noreply.github.com>
2024-05-14 00:27:07 +0400

    create accordion

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,9 +1,42 @@
+import React from "react";
 import "./accordion.scss";
 
 export default function Accordion() {
+  const accordionData = [
+    {
+      title: "Fiabilité",
+      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+          laborum cupiditate possimus labore, hic temporibus velit dicta earum
+          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+          voluptatem.`,
+    },
+    {
+      title: "Respect",
+      content: `content2`,
+    },
+    {
+      title: "Service",
+      content: `content3`,
+    },
+    {
+      title: "Sécurité",
+      content: `content4`,
+    },
+  ];
+  const { title, content } = accordionData;
   return (
-    <div className="wrapper">
-      <div className="accordion">{data.map(item, i)}</div>
-    </div>
+    <React.Fragment>
+      <div className="accordion">
+        <div className="accordion-item">
+          <div className="accordion-title">
+            <div>{title}</div>
+            <div>
+              <i className="fa-solid fa-chevron-up"></i>
+            </div>
+          </div>
+          <div className="accordion-content">{content}</div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
